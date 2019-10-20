15ea091f2e1e9440ff98bcbb6384f3b6f2303e3d
vector-of-bool <vectorofbool@gmail.com>
2019-10-19 18:17:50 -0600

    Common paths for each system

---
+++ src/dds/util/paths.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <dds/util/fs.hpp>
+
+namespace dds {
+
+fs::path user_home_dir();
+fs::path user_data_dir();
+fs::path user_cache_dir();
+fs::path user_config_dir();
+
+inline fs::path dds_data_dir() { return user_data_dir() / "ddslim"; }
+inline fs::path dds_cache_dir() { return user_cache_dir() / "ddslim-cache"; }
+inline fs::path dds_config_dir() { return user_config_dir() / "ddslim"; }
+
+}  // namespace dds
