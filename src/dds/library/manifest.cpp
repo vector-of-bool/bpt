bb3c1b0bc1a29cd9c0ce792f6646ed2c24518a86
vector-of-bool <vectorofbool@gmail.com>
2020-03-04 22:18:11 -0700

    Use semester for JSON processing

--- src/dds/library/manifest.cpp
+++ src/dds/library/manifest.cpp
@@ -3,11 +3,11 @@
 #include <dds/dym.hpp>
 #include <dds/error/errors.hpp>
 #include <dds/util/algo.hpp>
-#include <dds/util/json5_read.hpp>
 #include <libman/parse.hpp>
 
 #include <json5/parse_data.hpp>
 #include <range/v3/view/transform.hpp>
+#include <semester/decomp.hpp>
 #include <spdlog/spdlog.h>
 
 using namespace dds;
@@ -42,24 +42,58 @@ library_manifest library_manifest::load_from_file(path_ref fpath) {
     }
 
     library_manifest lib;
-    using namespace json_read::ops;
-    json_read::decompose(  //
-        data.as_object(),
-        object(key("name", require_string(put_into{lib.name}, "`name` must be a string")),
-               key("uses",
-                   array_each{require_string(
-                       [&](auto&& uses) {
-                           lib.uses.push_back(lm::split_usage_string(uses.as_string()));
-                           return json_read::accept_t{};
-                       },
-                       "All `uses` items must be strings")}),
-               key("links",
-                   array_each{require_string(
-                       [&](auto&& links) {
-                           lib.links.push_back(lm::split_usage_string(links.as_string()));
-                           return json_read::accept_t{};
-                       },
-                       "All `links` items must be strings")})));
+    using namespace semester::decompose_ops;
+    auto res = semester::decompose(  //
+        data,
+        try_seq{require_type<json5::data::mapping_type>{
+                    "The root of the library manifest must be an object (mapping)"},
+                mapping{
+                    if_key{"name",
+                           require_type<std::string>{"`name` must be a string"},
+                           put_into{lib.name}},
+                    if_key{"uses",
+                           require_type<json5::data::array_type>{
+                               "`uses` must be an array of usage requirements"},
+                           for_each{
+                               require_type<std::string>{"`uses` elements must be strings"},
+                               [&](auto&& uses) {
+                                   lib.uses.push_back(lm::split_usage_string(uses.as_string()));
+                                   return semester::dc_accept;
+                               },
+                           }},
+                    if_key{"links",
+                           require_type<json5::data::array_type>{
+                               "`links` must be an array of usage requirements"},
+                           for_each{
+                               require_type<std::string>{"`links` elements must be strings"},
+                               [&](auto&& links) {
+                                   lib.links.push_back(lm::split_usage_string(links.as_string()));
+                                   return semester::dc_accept;
+                               },
+                           }},
+                }});
+    auto rej = std::get_if<semester::dc_reject_t>(&res);
+    if (rej) {
+        throw_user_error<errc::invalid_lib_manifest>(rej->message);
+    }
+    // using namespace json_read::ops;
+    // json_read::decompose(  //
+    //     data.as_object(),
+    //     object(key("name", require_string(put_into{lib.name}, "`name` must be a string")),
+    //            key("uses",
+    //                array_each{require_string(
+    //                    [&](auto&& uses) {
+    //                        lib.uses.push_back(lm::split_usage_string(uses.as_string()));
+    //                        return json_read::accept_t{};
+    //                    },
+    //                    "All `uses` items must be strings")}),
+    //            key("links",
+    //                array_each{require_string(
+    //                    [&](auto&& links) {
+    //                        lib.links.push_back(lm::split_usage_string(links.as_string()));
+    //                        return json_read::accept_t{};
+    //                    },
+    //                    "All `links` items must be strings")})));
 
     if (lib.name.empty()) {
         throw_user_error<errc::invalid_lib_manifest>(
--- src/dds/package/manifest.cpp
+++ src/dds/package/manifest.cpp
@@ -2,13 +2,13 @@
 
 #include <dds/dym.hpp>
 #include <dds/error/errors.hpp>
-#include <dds/util/json5_read.hpp>
 #include <dds/util/string.hpp>
 #include <libman/parse.hpp>
 
 #include <range/v3/view/split.hpp>
 #include <range/v3/view/split_when.hpp>
 #include <range/v3/view/transform.hpp>
+#include <semester/decomp.hpp>
 #include <spdlog/spdlog.h>
 
 #include <json5/parse_data.hpp>
@@ -78,62 +78,82 @@ package_manifest package_manifest::load_from_file(const fs::path& fpath) {
         throw_user_error<errc::invalid_pkg_manifest>("Root value must be an object");
     }
 
-    const auto&      obj = data.as_object();
     package_manifest ret;
-
-    using namespace json_read::ops;
-    json_read::decompose(
-        obj,
-        object(
-            key("name", require_string(put_into{ret.pkg_id.name}, "`name` must be a string")),
-            key("namespace",
-                require_string(put_into{ret.namespace_}, "`namespace` must be a string")),
-            ignore_key{"$schema"},
-            key("version",
-                require_string(
+    using namespace semester::decompose_ops;
+    auto res = semester::decompose(  //
+        data,
+        try_seq{
+            require_type<json5::data::mapping_type>{
+                "The root of a package manifest must be an object (mapping)"},
+            mapping{
+                if_key{"$schema", just_accept},
+                if_key{
+                    "name",
+                    require_type<std::string>{"`name` must be a string"},
+                    put_into{ret.pkg_id.name},
+                },
+                if_key{
+                    "namespace",
+                    require_type<std::string>{"`namespace` must be a string"},
+                    put_into{ret.namespace_},
+                },
+                if_key{
+                    "version",
+                    require_type<std::string>{"`version` must be a string"},
                     [&](auto&& version_str_) {
                         auto& version      = version_str_.as_string();
                         ret.pkg_id.version = semver::version::parse(version);
-                        return json_read::accept_t{};
+                        return semester::dc_accept;
                     },
-                    "`version` must be a string")),
-            key("depends", object([&](auto key, auto&& range_str_) {
-                    auto pkg_name = std::string(key);
-                    if (!range_str_.is_string()) {
-                        throw_user_error<errc::invalid_pkg_manifest>(
-                            "Dependency for '{}' must be a range string", pkg_name);
-                    }
-                    try {
-                        auto       rng = semver::range::parse_restricted(range_str_.as_string());
-                        dependency dep{std::string(pkg_name), {rng.low(), rng.high()}};
-                        ret.dependencies.push_back(std::move(dep));
-                    } catch (const semver::invalid_range&) {
-                        throw_user_error<errc::invalid_version_range_string>(
-                            "Invalid version range string '{}' in dependency declaration for '{}'",
-                            range_str_.as_string(),
-                            pkg_name);
-                    }
-                    return json_read::accept_t{};
-                })),
-            key("test_driver",
-                require_string(
-                    [&](auto&& test_driver_str_) {
-                        auto& test_driver = test_driver_str_.as_string();
-                        if (test_driver == "Catch-Main") {
-                            ret.test_driver = test_lib::catch_main;
-                        } else if (test_driver == "Catch") {
-                            ret.test_driver = test_lib::catch_;
-                        } else {
-                            auto dym = *did_you_mean(test_driver, {"Catch-Main", "Catch"});
-                            throw_user_error<errc::unknown_test_driver>(
-                                "Unknown 'test_driver' '{}' (Did you mean '{}'?)",
-                                test_driver,
-                                dym);
+                },
+                if_key{
+                    "depends",
+                    require_type<json5::data::mapping_type>{
+                        "`depends` must be a mapping between package names and version ranges"},
+                    mapping{[&](auto pkg_name, auto&& range_str_) {
+                        if (!range_str_.is_string()) {
+                            throw_user_error<errc::invalid_pkg_manifest>(
+                                "Dependency for '{}' must be a range string", pkg_name);
                         }
-                        return json_read::accept_t{};
-                    },
-                    "`test_driver` must be a valid test driver name string")),
-            reject_key));
+                        try {
+                            auto rng = semver::range::parse_restricted(range_str_.as_string());
+                            dependency dep{std::string(pkg_name), {rng.low(), rng.high()}};
+                            ret.dependencies.push_back(std::move(dep));
+                        } catch (const semver::invalid_range&) {
+                            throw_user_error<errc::invalid_version_range_string>(
+                                "Invalid version range string '{}' in dependency declaration for "
+                                "'{}'",
+                                range_str_.as_string(),
+                                pkg_name);
+                        }
+                        return semester::dc_accept;
+                    }},
+                },
+                if_key{"test_driver",
+                       require_type<std::string>{"`test_driver` must be a string"},
+                       [&](auto&& test_driver_str_) {
+                           auto& test_driver = test_driver_str_.as_string();
+                           if (test_driver == "Catch-Main") {
+                               ret.test_driver = test_lib::catch_main;
+                           } else if (test_driver == "Catch") {
+                               ret.test_driver = test_lib::catch_;
+                           } else {
+                               auto dym = *did_you_mean(test_driver, {"Catch-Main", "Catch"});
+                               throw_user_error<errc::unknown_test_driver>(
+                                   "Unknown 'test_driver' '{}' (Did you mean '{}'?)",
+                                   test_driver,
+                                   dym);
+                           }
+                           return semester::dc_accept;
+                       }},
+                [&](auto key, auto&&) {
+                    return semester::dc_reject_t{
+                        fmt::format("Unknown key `{}` in package manifest", key)};
+                }}});
+    auto rej = std::get_if<semester::dc_reject_t>(&res);
+    if (rej) {
+        throw_user_error<errc::invalid_pkg_manifest>(rej->message);
+    }
 
     if (ret.pkg_id.name.empty()) {
         throw_user_error<errc::invalid_pkg_manifest>("The 'name' field is required.");
