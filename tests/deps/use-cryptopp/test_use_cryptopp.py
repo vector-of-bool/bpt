6f7e1c98f20e166c7564a8f6f9b6307b57aec0e6
vector-of-bool <colby.pike@storagecraft.com>
2020-07-07 12:25:04 -0600

    Wrong test name for Crypto++

--- tests/deps/use-cryptopp/test_use_cryptopp.py
+++ tests/deps/use-cryptopp/test_use_cryptopp.py
@@ -3,7 +3,7 @@ from tests import DDS
 from dds_ci import proc
 
 
-def test_get_build_use_libsodium(dds: DDS):
+def test_get_build_use_cryptopp(dds: DDS):
     dds.catalog_import(dds.source_root / 'catalog.json')
     tc_fname = 'gcc.tc.jsonc' if 'gcc' in dds.default_builtin_toolchain else 'msvc.tc.jsonc'
     tc = str(dds.test_dir / tc_fname)
