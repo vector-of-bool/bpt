0122cc8946f723ec8a5aefae9018181daceb67a7
vector-of-bool <vectorofbool@gmail.com>
2020-07-07 22:08:29 -0600

    Change dependency parsing to not expect a space in the dep string

--- tests/deps/build-deps/test_build_deps.py
+++ tests/deps/build-deps/test_build_deps.py
@@ -14,7 +14,7 @@ def test_build_deps_from_file(dds: DDS):
 def test_build_deps_from_cmd(dds: DDS):
     assert not dds.deps_build_dir.is_dir()
     dds.catalog_import(dds.source_root / 'catalog.json')
-    dds.build_deps(['neo-sqlite3 =0.2.2'])
+    dds.build_deps(['neo-sqlite3=0.2.2'])
     assert (dds.deps_build_dir / 'neo-sqlite3@0.2.2').is_dir()
     assert (dds.scratch_dir / 'INDEX.lmi').is_file()
     assert (dds.deps_build_dir / '_libman/neo-sqlite3.lmp').is_file()
@@ -24,7 +24,7 @@ def test_build_deps_from_cmd(dds: DDS):
 def test_multiple_deps(dds: DDS):
     assert not dds.deps_build_dir.is_dir()
     dds.catalog_import(dds.source_root / 'catalog.json')
-    dds.build_deps(['neo-sqlite3 ^0.2.2', 'neo-sqlite3 ~0.2.0'])
+    dds.build_deps(['neo-sqlite3^0.2.2', 'neo-sqlite3~0.2.0'])
     assert (dds.deps_build_dir / 'neo-sqlite3@0.2.2').is_dir()
     assert (dds.scratch_dir / 'INDEX.lmi').is_file()
     assert (dds.deps_build_dir / '_libman/neo-sqlite3.lmp').is_file()
