0b9a7ce77ba4ee633d618e7f33bec71258f0b30c
vector-of-bool <vectorofbool@gmail.com>
2020-07-11 00:16:17 -0600

    CryptoPP has trouble on FreeBSD

--- tests/deps/use-cryptopp/test_use_cryptopp.py
+++ tests/deps/use-cryptopp/test_use_cryptopp.py
@@ -1,8 +1,14 @@
 from tests import DDS
+import platform
+
+import pytest
 
 from dds_ci import proc
 
 
+@pytest.mark.skipif(
+    platform.system() == 'FreeBSD',
+    reason='This one has trouble running on FreeBSD')
 def test_get_build_use_cryptopp(dds: DDS):
     dds.catalog_import(dds.source_root / 'catalog.json')
     tc_fname = 'gcc.tc.jsonc' if 'gcc' in dds.default_builtin_toolchain else 'msvc.tc.jsonc'
