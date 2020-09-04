8814945ece3709f325c4de688859574d2ebaa26b
vector-of-bool <vectorofbool@gmail.com>
2020-09-04 11:45:07 -0600

    Check that sdist exports are limited to relevant files

--- tests/sdist/sdist_test.py
+++ tests/sdist/sdist_test.py
@@ -22,3 +22,6 @@ def test_import_sdist_archive(dds: DDS):
     assert not repo_content_path.is_dir()
     dds.repo_import(dds.build_dir / 'foo@1.2.3.tar.gz')
     assert repo_content_path.is_dir()
+    assert repo_content_path.joinpath('library.jsonc').is_file()
+    # Excluded file will not be in the sdist:
+    assert not repo_content_path.joinpath('other-file.txt').is_file()
