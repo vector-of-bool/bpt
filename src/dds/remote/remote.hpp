e8b8e5cddd7ca4b6b83174d1daca9c28f4b83bae
vector-of-bool <vectorofbool@gmail.com>
2020-12-07 21:22:38 -0700

    MSVC catches access control oddities that GCC misses

--- src/dds/remote/remote.hpp
+++ src/dds/remote/remote.hpp
@@ -16,12 +16,12 @@ class remote_repository {
     std::string _name;
     neo::url    _base_url;
 
+public:
     remote_repository(std::string name, neo::url url)
         : _name(std::move(name))
         , _base_url(std::move(url)) {}
     remote_repository() = default;
 
-public:
     static remote_repository connect(std::string_view url);
 
     void store(neo::sqlite3::database_ref);
