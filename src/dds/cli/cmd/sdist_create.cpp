c960646256a93f4b018231e4420fad0e68c66cc9
vector-of-bool <vectorofbool@gmail.com>
2020-12-29 19:01:10 -0700

    Clean up sdist-create output

--- src/dds/cli/cmd/sdist_create.cpp
+++ src/dds/cli/cmd/sdist_create.cpp
@@ -5,7 +5,10 @@
 
 #include <boost/leaf/common.hpp>
 #include <boost/leaf/handle_exception.hpp>
+#include <fansi/styled.hpp>
 #include <fmt/core.h>
+
+using namespace fansi::literals;
 
 namespace dds::cli::cmd {
 
@@ -23,20 +26,27 @@ int sdist_create(const options& opts) {
             auto default_filename = fmt::format("{}.tar.gz", pkg_man.id.to_string());
             auto filepath         = opts.out_path.value_or(fs::current_path() / default_filename);
             create_sdist_targz(filepath, params);
+            dds_log(info,
+                    "Created source dirtribution archive: .bold.cyan[{}]"_styled,
+                    filepath.string());
             return 0;
         },
         [&](boost::leaf::bad_result, e_missing_file missing, e_human_message msg) {
-            dds_log(error,
-                    "A required file is missing for creating a source distribution for [{}]",
-                    params.project_dir.string());
-            dds_log(error, "Error: {}", msg.value);
-            dds_log(error, "Missing file: {}", missing.path.string());
+            dds_log(
+                error,
+                "A required file is missing for creating a source distribution for [.bold.yellow[{}]]"_styled,
+                params.project_dir.string());
+            dds_log(error, "Error: .bold.yellow[{}]"_styled, msg.value);
+            dds_log(error, "Missing file: .bold.red[{}]"_styled, missing.path.string());
             write_error_marker("no-package-json5");
             return 1;
         },
         [&](std::error_code ec, e_human_message msg, boost::leaf::e_file_name file) {
-            dds_log(error, "Error: {}", msg.value);
-            dds_log(error, "Failed to access file [{}]: {}", file.value, ec.message());
+            dds_log(error, "Error: .bold.red[{}]"_styled, msg.value);
+            dds_log(error,
+                    "Failed to access file [.bold.red[{}]]: .br.yellow[{}]",
+                    file.value,
+                    ec.message());
             write_error_marker("failed-package-json5-scan");
             return 1;
         });
