4d65b6bb885dd11f28fcd919b448130fd1ab328b
Justin Bassett <jbassett271@gmail.com>
2021-04-30 00:37:33 -0700

    Fix test for public includes

--- tests/isolated_headers/test_isolated_headers.py
+++ tests/isolated_headers/test_isolated_headers.py
@@ -15,12 +15,12 @@ def test_dependent_src_header_fails(project_opener: ProjectOpener, capfd) -> Non
     assert 'bad_src.hpp' in errmsg
 
 
-# def test_dependent_include_header_fails(project_opener: ProjectOpener, capfd) -> None:
-#     proj = project_opener.open('bad_proj')
-#     with pytest.raises(subprocess.CalledProcessError):
-#         proj.build()
-#     out = capfd.readouterr()
-#     errmsg = out.out + '\n' + out.err
+def test_dependent_include_header_fails(project_opener: ProjectOpener, capfd) -> None:
+    proj = project_opener.open('bad_proj_include')
+    with pytest.raises(subprocess.CalledProcessError):
+        proj.build()
+    out = capfd.readouterr()
+    errmsg = out.out + '\n' + out.err
 
-#     assert 'Syntax check failed' in errmsg
-#     assert 'depends_src.hpp' in errmsg
+    assert 'Syntax check failed' in errmsg
+    assert 'depends_src.hpp' in errmsg
