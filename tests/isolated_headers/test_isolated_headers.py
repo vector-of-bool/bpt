a1954217d66d7d84bbfec0a8991e07570a10794c
Justin Bassett <jbassett271@gmail.com>
2021-04-30 00:32:55 -0700

    Use capfd instead of capsys
    capfd captures everything written to stdout rather than just to the
    python sys.stdout object.

--- tests/isolated_headers/test_isolated_headers.py
+++ tests/isolated_headers/test_isolated_headers.py
@@ -4,22 +4,22 @@ import subprocess
 from dds_ci.testing import ProjectOpener
 
 
-def test_dependent_src_header_fails(project_opener: ProjectOpener, capsys) -> None:
+def test_dependent_src_header_fails(project_opener: ProjectOpener, capfd) -> None:
     proj = project_opener.open('bad_proj')
     with pytest.raises(subprocess.CalledProcessError):
         proj.build()
-    out = capsys.readouterr()
+    out = capfd.readouterr()
     errmsg = out.out + '\n' + out.err
 
     assert 'Syntax check failed' in errmsg
     assert 'bad_src.hpp' in errmsg
 
 
-# def test_dependent_include_header_fails(project_opener: ProjectOpener, capsys) -> None:
+# def test_dependent_include_header_fails(project_opener: ProjectOpener, capfd) -> None:
 #     proj = project_opener.open('bad_proj')
 #     with pytest.raises(subprocess.CalledProcessError):
 #         proj.build()
-#     out = capsys.readouterr()
+#     out = capfd.readouterr()
 #     errmsg = out.out + '\n' + out.err
 
 #     assert 'Syntax check failed' in errmsg
