b3e715aa5a9152032176f077d8a4b7105b724ad5
Justin Bassett <jbassett271@gmail.com>
2021-08-10 15:11:56 -0700

    Embed openssl

--- tools/get-win-openssl.ps1
+++ tools/get-win-openssl.ps1
@@ -9,16 +9,7 @@ $root_dir = Split-Path -Parent $tools_dir
 $build_dir = Join-Path $root_dir "_build"
 New-Item -ItemType Container $build_dir -ErrorAction Ignore
 
-$local_tgz = Join-Path $build_dir "openssl.tgz"
-
-# This is the path to the release static vs2019 x64 build of OpenSSL in bintray
-$conan_ssl_path = "_/openssl/1.1.1h/_/8aee28bb7f844aa2e8e9f2b9e988027e/package/a79a557254fabcb77849dd623fed97c9c5ab7651/c200a0954463ed55a55d99809c0dba96"
-$openssl_url = "https://center.conan.io/artifactory/api/conan/conan-center/v1/files/$conan_ssl_path/conan_package.tgz"
-
-Write-Host "Downloading OpenSSL for Windows"
-Invoke-WebRequest `
-    -Uri $openssl_url `
-    -OutFile $local_tgz
+$local_tgz = Join-Path $tools_dir "windows-openssl.tgz"
 
 $openssl_tree = Join-Path $root_dir "external/OpenSSL"
 Write-Host "Expanding OpenSSL archive..."
