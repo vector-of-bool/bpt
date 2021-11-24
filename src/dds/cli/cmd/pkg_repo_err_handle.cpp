54907e3fe501270f5dbd169434bfb9ba69015b98
vector-of-bool <vectorofbool@gmail.com>
2021-11-24 16:18:07 -0700

    Fix old LEAF usage

--- src/dds/cli/cmd/pkg_repo_err_handle.cpp
+++ src/dds/cli/cmd/pkg_repo_err_handle.cpp
@@ -10,7 +10,7 @@
 #include <dds/util/log.hpp>
 #include <dds/util/result.hpp>
 
-#include <boost/leaf/handle_exception.hpp>
+#include <boost/leaf.hpp>
 #include <fansi/styled.hpp>
 #include <json5/parse_data.hpp>
 #include <neo/sqlite3/error.hpp>
@@ -20,13 +20,7 @@ using namespace fansi::literals;
 
 int dds::cli::cmd::handle_pkg_repo_remote_errors(std::function<int()> fn) {
     return boost::leaf::try_catch(
-        [&] {
-            try {
-                return fn();
-            } catch (...) {
-                dds::capture_exception();
-            }
-        },
+        fn,
         [](neo::url_validation_error url_err, neo::url bad_url) {
             dds_log(error, "Invalid URL [{}]: {}", bad_url.to_string(), url_err.what());
             return 1;
@@ -58,20 +52,20 @@ int dds::cli::cmd::handle_pkg_repo_remote_errors(std::function<int()> fn) {
             dds_log(error,
                     "Error accessing remote database [.br.red[{}]]: {}"_styled,
                     url.to_string(),
-                    e.value().what());
+                    e.matched.what());
             return 1;
         },
         [](boost::leaf::catch_<neo::sqlite3::error> e) {
-            dds_log(error, "Unexpected database error: {}", e.value().what());
+            dds_log(error, "Unexpected database error: {}", e.matched.what());
             return 1;
         },
-        [](dds::e_system_error_exc e, dds::network_origin conn) {
+        [](const std::system_error& e, dds::network_origin conn) {
             dds_log(error,
                     "Error communicating with [.br.red[{}://{}:{}]]: {}"_styled,
                     conn.protocol,
                     conn.hostname,
                     conn.port,
-                    e.message);
+                    e.code().message());
             return 1;
         },
         [](matchv<pkg_repo_subcommand::remove>, e_nonesuch missing) {
